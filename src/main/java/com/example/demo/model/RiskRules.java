e1706dfaaf192e5f0c9ef47f655d33eab37d7c58
bala-amypo <bala@amypo.in>
2025-12-18 15:28:07 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRules.java
+++ src/main/java/com/example/demo/model/RiskRules.java
@@ -1,5 +1,10 @@
 package com.example.demo.model;
 
 public class Riskrules{
-   
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private Long id;
+   private String ruleName;
+   private String ruleType;
+   private 
 }
