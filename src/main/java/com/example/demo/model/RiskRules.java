f9b8cc9084d61f3bffda9f773e9c52ad03a70074
bala-amypo <bala@amypo.in>
2025-12-18 16:40:07 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRules.java
+++ src/main/java/com/example/demo/model/RiskRules.java
@@ -1,36 +1,61 @@
 package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import java.util.LocalDateTime;
-import jakarta.persistence .PrePersist;
-import jakarta.persistence.Column;
-public class Riskrules{
-   @Id
-   @GeneratedValue(strategy=GenerationType.IDENTITY)
-   private Long id;
-   @Column(unique=true)
-   private String ruleName;
-   private String ruleType;
-   private Integer threshold;
-   private Integr scoreImpact;
-   private LocalDateTime createdAt;
-   @PrePersist
-   void validate(){
-    if(threshold <0 || scoreImpact <0 ){
-        throw new RuntimeException("Invalid rule values");
+
+import jakarta.persistence.*en;
+import java.time.LocalDateTime;
+@Entity
+public class RiskRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String ruleName;
+
+    private String ruleType;
+    private Integer threshold;
+    private Integer scoreImpact;
+    private LocalDateTime createdAt;
+
+    public RiskRule() {
     }
-    createdAt = LocalDateTime.now();
-   }
-   public Long getId(){
-    return id;
-   }
-   public void setId(Long id){
-    this.id=id;
-   }
-   public String getRuleName(){
-    return ruleName;
-   }
-   public void 
+
+    public RiskRule(String ruleName, String ruleType, Integer threshold, Integer scoreImpact) {
+        this.ruleName = ruleName;
+        this.ruleType = ruleType;
+        this.threshold = threshold;
+        this.scoreImpact = scoreImpact;
+    }
+
+    @PrePersist
+    void validate() {
+        if (threshold < 0 || scoreImpact < 0) {
+            throw new RuntimeException("Invalid threshold or scoreImpact");
+        }
+        createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public String getRuleType() {
+        return ruleType;
+    }
+
+    public Integer getThreshold() {
+        return threshold;
+    }
+
+    public Integer getScoreImpact() {
+        return scoreImpact;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
