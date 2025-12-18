dadeea76df444f3ee6eb2171843e5e0b193e6f73
bala-amypo <bala@amypo.in>
2025-12-18 15:34:07 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -1,5 +1,14 @@
 package com.example.demo.model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.LocalDateTime;
+import jakarta.persistence .PrePersist;
+@Entity
 public class RiskScore{
-  
+  @Id
+  @GeneratedValue(strategy=GenerationType.INDENTITY)
+  private Long id;
+  private Integer 
 }
