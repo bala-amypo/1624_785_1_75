5fabd4681f5fa581d90331ab817a54709e10c5de
bala-amypo <bala@amypo.in>
2025-12-18 01:27:00 +0000

    User commit

--- src/main/java/com/example/demo/util/RiskLevelUtils.java
+++ src/main/java/com/example/demo/util/RiskLevelUtils.java
@@ -1,1 +1,8 @@
-package com.example.demo.util.RiskLevelUtils
+package com.example.demo.util.RiskLevelUtils;
+public class RiskLevelUtils{
+    public static String determineRiskLevel(int score){
+        if(score>=0 && score<=19){
+            return "LOW";
+        }else if(score>=20&& score)
+    }
+}
