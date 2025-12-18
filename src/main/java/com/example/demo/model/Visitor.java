9ec8eb4b9f2c47efea2aee981d83f67d2b65a106
bala-amypo <bala@amypo.in>
2025-12-18 16:34:07 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -3,21 +3,20 @@ import jakarta.persistence.Entity;
 import java.time.LocalDateTime;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
-import 
+import jakarta.persistence.Id;
+import jakarta.persitence.PrePersist;
 @Entity
 public class Visitor {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String fullName;
     private String email;
     private String phone;
     private String idProof;
     private LocalDateTime createdAt;
+    public Visitor() { 
 
-    public Visitor() {
     }
 
     public Visitor(String fullName, String phone, String idProof, String email) {
@@ -26,7 +25,6 @@ public class Visitor {
         this.idProof = idProof;
         this.email = email;
     }
-
     @PrePersist
     void validate() {
         if (fullName == null || phone == null || idProof == null) {
@@ -34,43 +32,33 @@ public class Visitor {
         }
         createdAt = LocalDateTime.now();
     }
-
     public Long getId() {
         return id;
     }
-
     public String getFullName() {
         return fullName;
     }
-
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getIdProof() {
         return idProof;
     }
-
     public void setIdProof(String idProof) {
         this.idProof = idProof;
     }
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
