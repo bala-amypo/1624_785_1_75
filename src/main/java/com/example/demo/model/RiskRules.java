d0f71e1b7077cf43eb873286113af71834db192c
bala-amypo <bala@amypo.in>
2025-12-18 15:32:08 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRules.java
+++ src/main/java/com/example/demo/model/RiskRules.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.LocalDateTime;
+import jakarta.persistence .PrePersist;
+import jakarta.persistence.Column;
 public class Riskrules{
    @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
