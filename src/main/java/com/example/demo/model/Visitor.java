e7c5bf1fd9e01935c2725385c0deaf6be8439da2
bala-amypo <bala@amypo.in>
2025-12-18 16:04:07 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -25,6 +25,17 @@ public class Visitor{
     public Long getId(){
         return id;
     }
-    public String 
-
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    
 }
