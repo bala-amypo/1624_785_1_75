4117f3d64838397f78df160b32c9515d22863131
bala-amypo <bala@amypo.in>
2025-12-18 01:44:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitLogRepository.java
+++ src/main/java/com/example/demo/repository/VisitLogRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Jparepository;
+import com.example.demo.model.VisitLog;
 @Repository
-public interface VisitLogRepository extends Jparepository<
+public interface VisitLogRepository extends Jparepository<VisitLog,Long>{
+
+}
