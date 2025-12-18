003a6f0d017e2cfdae934850561cffd7b3e056d0
bala-amypo <bala@amypo.in>
2025-12-18 16:47:41 +0000

    User commit

--- src/main/java/com/example/demo/model/ScoreAuditLog.java
+++ src/main/java/com/example/demo/model/ScoreAuditLog.java
@@ -1,5 +1,56 @@
 package com.example.demo.model;
-
-public class ScoreAuditLog{
-    
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+public class ScoreAuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Visitor visitor;
+    @ManyToOne
+    private RiskRule appliedRule;
+    private Integer scoreChange;
+    private String reason;
+    private LocalDateTime loggedAt;
+    public ScoreAuditLog() {
+    }
+    public ScoreAuditLog(Integer scoreChange, String reason) {
+        this.scoreChange = scoreChange;
+        this.reason = reason;
+    }
+    @PrePersist
+    void validate() {
+        if (scoreChange < 0) {
+            throw new RuntimeException("Invalid scoreChange");
+        }
+        if (reason == null) {
+            throw new RuntimeException("reason required");
+        }
+        loggedAt = LocalDateTime.now();
+    }
+    public Long getId() {
+        return id;
+    }
+    public Visitor getVisitor() {
+        return visitor;
+    }
+    public void setVisitor(Visitor visitor) {
+        this.visitor = visitor;
+    }
+    public RiskRule getAppliedRule() {
+        return appliedRule;
+    }
+    public void setAppliedRule(RiskRule appliedRule) {
+        this.appliedRule = appliedRule;
+    }
+    public Integer getScoreChange() {
+        return scoreChange;
+    }
+    public String getReason() {
+        return reason;
+    }
+    public LocalDateTime getLoggedAt() {
+        return loggedAt;
+    }
 }
