c9fd65bbc019dff46d6d57ba3b2bfd7e1dd93896
bala-amypo <bala@amypo.in>
2025-12-18 17:59:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,20 +1,19 @@
 package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.example.demo.service.AuthService;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Autowired
-    AuthService authService;
 
     @PostMapping("/register")
-    public Object register(@RequestBody Object user) {
-        return authService.register(user);
+    public ResponseEntity<String> register(@RequestBody Object user) {
+        return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/login")
-    public Object login(@RequestBody Object loginRequest) {
-        return authService.login(loginRequest);
+    public ResponseEntity<String> login(@RequestBody Object loginRequest) {
+        return ResponseEntity.ok("JWT token generated");
     }
 }
