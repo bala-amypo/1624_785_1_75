5b5cc73f4453d3b9bd089738c7f0592552f3b38a
bala-amypo <bala@amypo.in>
2025-12-18 17:16:07 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitorService.java
+++ src/main/java/com/example/demo/service/VisitorService.java
@@ -1,5 +1,22 @@
 package com.example.demo.service;
-
-public class VisitorService{
-    
+import com.example.demo.model.Visitor;
+import com.example.demo.repository.VisitorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class VisitorService {
+    private final VisitorRepository visitorRepository;
+    public VisitorService(VisitorRepository visitorRepository) {
+        this.visitorRepository = visitorRepository;
+    }
+    public Visitor createVisitor(Visitor visitor) {
+        return visitorRepository.save(visitor);
+    }
+    public Visitor getVisitor(Long id) {
+        return visitorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Visitor not found"));
+    }
+    public List<Visitor> getAllVisitors() {
+        return visitorRepository.findAll();
+    }
 }
