09331e88f4ef49b67bb325db4c111455ce9e76bf
bala-amypo <bala@amypo.in>
2025-12-18 01:54:34 +0000

    User commit

--- src/main/java/com/example/demo/model/VisitLog.java
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -1,1 +1,10 @@
 package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
+import jakarta.perisitence.Id;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
+public class VisitLog{
+
+}
