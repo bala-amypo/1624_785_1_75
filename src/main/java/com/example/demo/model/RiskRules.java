47866e4b7f7a0f7af8b5a891666f5a752f18cbda
bala-amypo <bala@amypo.in>
2025-12-18 15:31:07 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRules.java
+++ src/main/java/com/example/demo/model/RiskRules.java
@@ -4,7 +4,17 @@ public class Riskrules{
    @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
    private Long id;
+   @Column(unique=true)
    private String ruleName;
    private String ruleType;
-   private 
+   private Integer threshold;
+   private Integr scoreImpact;
+   private LocalDateTime createdAt;
+   @PrePersist
+   void validate(){
+    if(threshold <0 || scoreImpact <0 ){
+        throw new RuntimeException("Invalid rule values");
+    }
+    createdAt = LocalDateTime.now();
+   }
 }
