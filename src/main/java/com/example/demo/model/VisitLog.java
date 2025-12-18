945054d23de4bae320ec3769fe7d0f1e1687ac91
bala-amypo <bala@amypo.in>
2025-12-18 15:16:07 +0000

    User commit

--- src/main/java/com/example/demo/model/VisitLog.java
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -2,5 +2,14 @@ package com.example.demo.model;
 @Entity
 public class VisitLog{
     @Id
-    @
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+      private Long id;
+      private LocalDateTime entryTime;
+       private LocalDateTime exitTime;
+      private String purpose;
+      private String location;
+      @PrePersist
+      void validate(){
+        if(purpose)
+      }
 }
