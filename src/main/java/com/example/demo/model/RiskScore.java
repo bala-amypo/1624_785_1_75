671bc63361e8c1aa500a9d5d06e680376ad462e9
bala-amypo <bala@amypo.in>
2025-12-18 16:46:04 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -1,24 +1,50 @@
 package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import java.util.LocalDateTime;
-import jakarta.persistence .PrePersist;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 @Entity
-public class RiskScore{
-  @Id
-  @GeneratedValue(strategy=GenerationType.INDENTITY)
-  private Long id;
-  private Integer totalScore;
-  private String riskLevel;
-  private String totalScore;
-  private LocalDateTime evaluvatedAt;
-  @PrePersist
-  void calculateRisk(){
-    if(totalScore < 0){
-        throw new RuntimeException("Invalid score");
+public class RiskScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Visitor visitor;
+    private Integer totalScore;
+    private String riskLevel;
+    private LocalDateTime evaluatedAt;
+    public RiskScore() {
     }
-    
-  }
+    public RiskScore(Visitor visitor, Integer totalScore) {
+        this.visitor = visitor;
+        this.totalScore = totalScore;
+    }
+    @PrePersist
+    void calculateRisk() {
+        if (totalScore < 0) {
+            throw new RuntimeException("Invalid score");
+        }
+        riskLevel = RiskLevelUtils.determineRiskLevel(totalScore);
+        evaluatedAt = LocalDateTime.now();
+    }
+    public Long getId() {
+        return id;
+    }
+    public Visitor getVisitor() {
+        return visitor;
+    }
+    public void setVisitor(Visitor visitor) {
+        this.visitor = visitor;
+    }
+    public Integer getTotalScore() {
+        return totalScore;
+    }
+    public void setTotalScore(Integer totalScore) {
+        this.totalScore = totalScore;
+    }
+    public String getRiskLevel() {
+        return riskLevel;
+    }
+    public LocalDateTime getEvaluatedAt() {
+        return evaluatedAt;
+    }
 }
