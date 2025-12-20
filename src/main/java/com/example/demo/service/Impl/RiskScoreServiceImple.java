c1fba290c825cde23391df4ba5b5bb9bb068ae14
bala-amypo <bala@amypo.in>
2025-12-20 11:04:28 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskScoreServiceImple.java
+++ src/main/java/com/example/demo/service/Impl/RiskScoreServiceImple.java
@@ -10,10 +10,38 @@ import com.example.demo.exception.ResourceNotFoundException;
 public class RiskScoreServiceImpl implements RiskScoreService {
     private final RiskScoreRepository scoreRepo;
     private final VisitorRepository visitorRepo;
-    public RiskScoreServiceImpl(RiskScoreRepository scoreRepo, VisitorRepository visitorRepo) {
+    public RiskScoreServiceImpl(RiskScoreRepository scoreRepo,VisitorRepository visitorRepo) {
         this.scoreRepo = scoreRepo;
         this.visitorRepo = visitorRepo;
     }
-     public RiskScore evaluateVisitor(Long visitorId) {
+    @Override
+    public RiskScore evaluateVisitor(Long visitorId, Integer totalScore) {
         Visitor visitor = visitorRepo.findById(visitorId)
                 .orElseThrow(() -> new ResourceNotFoundException("Visitor not found"));
+        RiskScore riskScore = new RiskScore();
+        riskScore.setVisitor(visitor);
+        riskScore.setTotalScore(totalScore);
+
+        return scoreRepo.save(riskScore);
+    }
+
+    // READ by ID
+    @Override
+    public RiskScore getRiskScore(Long id) {
+        return scoreRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("RiskScore not found"));
+    }
+
+    // READ all
+    @Override
+    public List<RiskScore> getAllRiskScores() {
+        return scoreRepo.findAll();
+    }
+
+    // DELETE
+    @Override
+    public void deleteRiskScore(Long id) {
+        RiskScore score = getRiskScore(id);
+        scoreRepo.delete(score);
+    }
+}
