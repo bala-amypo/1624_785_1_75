5f57daf7c537b6cfc52fea74377ce5fdeca03096
bala-amypo <bala@amypo.in>
2025-12-20 14:59:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ScoreAuditLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ScoreAuditLogServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.service.impl;
+package com.example.demo.service.impl;
 import com.example.demo.model.ScoreAuditLog;
 import com.example.demo.model.RiskRule;
 import com.example.demo.model.Visitor;
