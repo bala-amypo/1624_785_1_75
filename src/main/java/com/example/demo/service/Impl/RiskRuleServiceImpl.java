86b14c09b1b91be7c06941665a9bb018cdc37f1c
bala-amypo <bala@amypo.in>
2025-12-20 11:14:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/RiskRuleServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.service.impl;
+import com.example.entity.RiskRule;
+import com.example.repository.RiskRuleRepository;
+import com.example.service.RiskRuleService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class RiskRuleServiceImpl implements RiskRuleService {
+    private final RiskRuleRepository riskRuleRepository;
+    public RiskRuleServiceImpl(RiskRuleRepository riskRuleRepository) {
+        this.riskRuleRepository = riskRuleRepository;
+    }
+    @Override
+    public RiskRule createRule(RiskRule rule) {
+        return riskRuleRepository.save(rule);
+    }
+    @Override
+    public List<RiskRule> getAllRules() {
+        return riskRuleRepository.findAll();
+    }
+    @Override
+    public RiskRule getRule(Long id) {
+        return riskRuleRepository.findById(id).orElse(null);
+    }
+}
