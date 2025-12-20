f522910e8c5879d0cc04043d8b2cf22cb6a74840
bala-amypo <bala@amypo.in>
2025-12-20 16:06:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
@@ -1,8 +1,8 @@
 package com.example.service.impl;
-import com.example.demo.model.RiskRules;
+import com.example.demo.model.RiskRule;
 import com.example.demo.model.RiskScore;
 import com.example.demo.model.Visitor;
-import com.example.demo.repository.RiskRulesRepository;
+import com.example.demo.repository.RiskRuleRepository;
 import com.example.demo.repository.RiskScoreRepository;
 import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.RiskScoreService;
@@ -11,17 +11,17 @@ import java.util.List;
 @Service
 public class RiskScoreServiceImpl implements RiskScoreService {
     private final RiskScoreRepository riskScoreRepository;
-    private final RiskRulesRepository riskRulesRepository;
+    private final RiskRuleRepository riskRuleRepository;
     private final VisitorRepository visitorRepository;
-    public RiskScoreServiceImpl(RiskScoreRepository riskScoreRepository,RiskRulesRepository riskRulesRepository,VisitorRepository visitorRepository) {
+    public RiskScoreServiceImpl(RiskScoreRepository riskScoreRepository,RiskRuleRepository riskRuleRepository,VisitorRepository visitorRepository) {
         this.riskScoreRepository = riskScoreRepository;
-        this.riskRulesRepository = riskRulesRepository;
+        this.riskRuleRepository = riskRuleRepository;
         this.visitorRepository = visitorRepository;
     }
     @Override
     public RiskScore evaluateVisitor(Long visitorId) {
         Visitor visitor = visitorRepository.findById(visitorId).orElse(null);
-        List<RiskRules> rules = riskRulesRepository.findAll();
+        List<RiskRule> rules = riskRuleRepository.findAll();
         int totalScore = 0;
         for (RiskRules rule : rules) {
             totalScore += rule.getScore();
