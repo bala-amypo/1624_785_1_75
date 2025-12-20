aed0426b5048e1139352df2b3486ecdf2bfb0e51
bala-amypo <bala@amypo.in>
2025-12-20 21:06:39 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
-// package com.example.demo.exception;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
-//     @ExceptionHandler(BadRequestException.class)
-//     public String handleBadRequest(BadRequestException ex) {
-//         return ex.getMessage();
-//     }
-//     @ExceptionHandler(ResourceNotFoundException.class)
-//     public String handleNotFound(ResourceNotFoundException ex) {
-//         return ex.getMessage();
-//     }
-// }
+package com.example.demo.exception;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public String handleBadRequest(BadRequestException ex) {
+        return ex.getMessage();
+    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String handleNotFound(ResourceNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
