90e661a91cad1c6bd304f57dcecf1a4e4b57f21d
bala-amypo <bala@amypo.in>
2025-12-20 14:24:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitLogController.java
+++ src/main/java/com/example/demo/controller/VisitLogController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
