86669ae79f8c776c09c4ec2fc02b204a11c2569e
bala-amypo <bala@amypo.in>
2025-12-20 13:45:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskScoreController.java
+++ src/main/java/com/example/demo/controller/RiskScoreController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.demo.model.RiskScore;
 import com.example.demo.service.RiskScoreService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 @RequestMapping("/api/risk-scores")
 public class RiskScoreController {
