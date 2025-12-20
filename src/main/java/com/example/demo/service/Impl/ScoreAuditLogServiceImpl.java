9033a1174bc5b1f42e192dad226416d7af88f233
bala-amypo <bala@amypo.in>
2025-12-20 11:19:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/ScoreAuditLogServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.service.Impl;
+import com.example.entity.ScoreAuditLog;
+import com.example.entity.RiskRule;
+import com.example.entity.Visitor;
+import com.example.repository.ScoreAuditLogRepository;
+import com.example.repository.RiskRuleRepository;
+import com.example.repository.VisitorRepository;
+import com.example.service.ScoreAuditLogService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class ScoreAuditLogServiceImpl implements ScoreAuditLogService {
+    private final ScoreAuditLogRepository auditLogRepository;
+    private final VisitorRepository visitorRepository;
+    private final RiskRuleRepository riskRuleRepository;
+    public ScoreAuditLogServiceImpl(ScoreAuditLogRepository auditLogRepository,VisitorRepository visitorRepository,RiskRuleRepository riskRuleRepository) {
+        this.auditLogRepository = auditLogRepository;
+        this.visitorRepository = visitorRepository;
+        this.riskRuleRepository = riskRuleRepository;
+    }
+    @Override
+    public ScoreAuditLog logScoreChange(Long visitorId, Long ruleId, ScoreAuditLog log) {
+        Visitor visitor = visitorRepository.findById(visitorId).orElse(null);
+        RiskRule rule = riskRuleRepository.findById(ruleId).orElse(null);
+        log.setVisitor(visitor);
+        log.setRiskRule(rule);
+        return auditLogRepository.save(log);
+    }
+    @Override
+    public List<ScoreAuditLog> getLogsByVisitor(Long visitorId) {
+        return auditLogRepository.findByVisitorId(visitorId);
+    }
+    @Override
+    public ScoreAuditLog getLog(Long id) {
+        return auditLogRepository.findById(id).orElse(null);
+    }
+}
