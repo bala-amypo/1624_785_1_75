093711f05990d72d86b62f00b286b1e74f3e4d55
bala-amypo <bala@amypo.in>
2025-12-20 11:23:27 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -8,28 +8,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     @Override
     public User register(User user) {
         return userRepository.save(user);
     }
-
     @Override
     public AuthResponse login(AuthRequest request) {
-
-        User user = userRepository.findByEmail(request.getEmail());
-
-        AuthResponse response = new AuthResponse();
+      User user = userRepository.findByEmail(request.getEmail());
+      AuthResponse response = new AuthResponse();
         response.setMessage("Login successful");
         response.setUser(user);
-
         return response;
     }
-
     @Override
     public User getByEmail(String email) {
         return userRepository.findByEmail(email);
