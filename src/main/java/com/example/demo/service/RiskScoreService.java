bd476d97d4eb931329125425d65ec344db18ac08
bala-amypo <bala@amypo.in>
2025-12-20 12:55:30 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskScoreService.java
+++ src/main/java/com/example/demo/service/RiskScoreService.java
@@ -2,7 +2,8 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.RiskScore;
 public interface RiskScoreService {
-    RiskScore evaluateVisitor(Long visitorId);
-    RiskScore getScoreForVisitor(Long visitorId);
-    List<RiskScore> getAllScores();
+    RiskScore evaluateVisitor(Long visitorId, Integer totalScore);
+    RiskScore getRiskScore(Long id);
+    List<RiskScore> getAllRiskScores();
+    void deleteRiskScore(Long id);
 }
