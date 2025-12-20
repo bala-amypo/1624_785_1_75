082d0f58c0b147df3baecf6b4078c893d8efb881
bala-amypo <bala@amypo.in>
2025-12-20 11:24:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,17 +1,13 @@
 package com.example.demo.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Object user) {
         return ResponseEntity.ok("User registered successfully");
     }
-
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Object loginRequest) {
         return ResponseEntity.ok("JWT token generated");
