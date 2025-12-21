0476fdb9227a60712ab50199a63fff95e39a2889
bala-amypo <bala@amypo.in>
2025-12-21 03:42:30 +0000

    User commit

--- src/main/java/com/example/demo/model/ScoreAuditLog.java
+++ src/main/java/com/example/demo/model/ScoreAuditLog.java
@@ -1,6 +1,17 @@
 package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 @Entity
