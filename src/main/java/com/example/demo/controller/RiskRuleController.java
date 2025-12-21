ca429aaa4b70d3c2f5b05312d5413c68964b897a
bala-amypo <bala@amypo.in>
2025-12-21 03:46:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskRuleController.java
+++ src/main/java/com/example/demo/controller/RiskRuleController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
-
 import com.example.demo.entity.RiskRule;
 import com.example.demo.service.RiskRuleService;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
