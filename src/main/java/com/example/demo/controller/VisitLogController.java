9da546504c31b348d4e5feef2e6264c59047c2b1
bala-amypo <bala@amypo.in>
2025-12-21 03:41:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitLogController.java
+++ src/main/java/com/example/demo/controller/VisitLogController.java
@@ -1,18 +1,4 @@
 package com.example.demo.controller;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/visit-logs")
--- src/main/java/com/example/demo/model/VisitLog.java
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -1,7 +1,19 @@
 package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
+
 @Entity
 @Getter @Setter
 @Builder
