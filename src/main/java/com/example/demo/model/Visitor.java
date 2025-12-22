c71ef835078dff610c985382db749219cc7c413d
bala-amypo <bala@amypo.in>
2025-12-22 13:33:47 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,31 +1,53 @@
+// 
+
 package com.example.demo.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 import java.time.LocalDateTime;
+
 @Entity
-@Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Visitor {
+
     @Id
     @GeneratedValue
     private Long id;
+
     private String fullName;
-    private String email;
     private String phone;
-    private String idProof;
     private LocalDateTime createdAt;
+
     @PrePersist
     void onCreate() {
         createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+     return id; 
+     }
+    public String getFullName() { 
+    return fullName;
+     }
+    public String getPhone() { 
+    return phone; 
+    }
+    public LocalDateTime getCreatedAt() {
+     return createdAt; 
+     }
+
+    public void setFullName(String fullName) {
+     this.fullName = fullName; 
+     }
+    public void setPhone(String phone) { 
+    this.phone = phone; 
     }
 }
