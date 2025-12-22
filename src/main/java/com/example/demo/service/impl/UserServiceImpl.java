2fef54ac1669d71856cd97e9b00f2a0e5aaa1b4f
bala-amypo <bala@amypo.in>
2025-12-22 12:38:46 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -32,7 +32,7 @@
 // }
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.User;
+import com.example.demo.model.User;
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
