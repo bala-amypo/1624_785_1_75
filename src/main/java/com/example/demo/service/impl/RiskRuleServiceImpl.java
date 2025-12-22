037718ded10a5bbb1e08cc390fb41a39f7343a15
bala-amypo <bala@amypo.in>
2025-12-22 12:25:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
@@ -1,26 +1,45 @@
+// 
 package com.example.demo.service.impl;
-import com.example.demo.model.RiskRule;
+
+import com.example.demo.entity.RiskRule;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.RiskRuleRepository;
 import com.example.demo.service.RiskRuleService;
-import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.stereotype.Service;
+
 @Service
 public class RiskRuleServiceImpl implements RiskRuleService {
-   private final RiskRuleRepository repository;
-   public RiskRuleServiceImpl(RiskRuleRepository repository) {
-        this.repository = repository;
+
+    private final RiskRuleRepository riskRuleRepository;
+
+    public RiskRuleServiceImpl(RiskRuleRepository riskRuleRepository) {
+        this.riskRuleRepository = riskRuleRepository;
     }
+
     @Override
     public RiskRule createRiskRule(RiskRule rule) {
-        return repository.save(rule);
+
+        if (rule.getThreshold() < 0 || rule.getScoreImpact() < 0) {
+            throw new BadRequestException("Threshold and scoreImpact must be non-negative");
+        }
+
+        if (riskRuleRepository.existsByRuleName(rule.getRuleName())) {
+            throw new BadRequestException("Rule name must be unique");
+        }
+
+        return riskRuleRepository.save(rule);
     }
+
     @Override
     public RiskRule getRiskRuleById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("RiskRule not found"));
+        return riskRuleRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Risk rule not found"));
     }
+
     @Override
     public List<RiskRule> getAllRiskRules() {
-        return repository.findAll();
+        return riskRuleRepository.findAll();
     }
 }
