9b563b404ceb47ac32daa2945da16d7827715905
bala-amypo <bala@amypo.in>
2025-12-22 12:39:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -27,7 +27,7 @@
 
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.Visitor;
+import com.example.demo.model.Visitor;
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.VisitorRepository;
