41611f37cca1fd443dd17faedc3a96cc2e7c492e
bala-amypo <bala@amypo.in>
2025-12-22 12:31:08 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRule.java
+++ src/main/java/com/example/demo/model/RiskRule.java
@@ -2,21 +2,23 @@ package com.example.demo.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RiskRule {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     @Column(unique = true)
     private String ruleName;
@@ -26,8 +28,6 @@ public class RiskRule {
     private LocalDateTime createdAt;
     @PrePersist
     void onCreate() {
-        if (threshold < 0 || scoreImpact < 0)
-            throw new RuntimeException("Invalid score");
         createdAt = LocalDateTime.now();
     }
 }
