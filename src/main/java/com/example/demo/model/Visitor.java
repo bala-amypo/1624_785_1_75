e05de837e05ec6a5af64578606785ef3c33ae1a0
bala-amypo <bala@amypo.in>
2025-12-26 15:21:56 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -3,61 +3,24 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Visitor {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
     private Long id;
 
-    private String name;
+    private String fullName;
+    private String email;
     private String phone;
+    private String idProof;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
 }
-
-
-// package com.example.demo.model;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.PrePersist;
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.NoArgsConstructor;
-// import java.time.LocalDateTime;
-// @Entity
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class Visitor {
-//     @Id
-//     @GeneratedValue
-//     private Long id;
-//     private String fullName;
-//     private String phone;
-//     private LocalDateTime createdAt;
-//     @PrePersist
-//     void onCreate() {
-//         createdAt = LocalDateTime.now();
-//     }
-//     public Long getId() {
-//      return id; 
-//      }
-//     public String getFullName() { 
-//     return fullName;
-//      }
-//     public String getPhone() { 
-//     return phone; 
-//     }
-//     public LocalDateTime getCreatedAt() {
-//      return createdAt; 
-//      }
-//      public void setFullName(String fullName) {
-//      this.fullName = fullName; 
-//      }
-//     public void setPhone(String phone) { 
-//     this.phone = phone; 
-//     }
-// }
