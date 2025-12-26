3ceb63cc0dafde1aaf7fcb4652ba1f475f8075a9
bala-amypo <bala@amypo.in>
2025-12-26 15:21:09 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -6,15 +6,11 @@ import lombok.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
     private Long id;
 
     private String email;
@@ -23,31 +19,3 @@ public class User {
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> roles;
 }
-
-// package com.example.demo.model;
-// import jakarta.persistence.Column;
-// import jakarta.persistence.ElementCollection;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
-// import java.util.Set;
-// @Entity
-// @Getter @Setter
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class User {
-//     @Id @GeneratedValue
-//     private Long id;
-//     @Column(unique = true)
-//     private String email;
-//     private String password;
-//     @ElementCollection
-//     private Set<String> role;
-// }
