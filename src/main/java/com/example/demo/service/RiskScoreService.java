1f3c510060b0650297b309052c61c4462325549d
bala-amypo <bala@amypo.in>
2025-12-26 15:27:33 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskScoreService.java
+++ src/main/java/com/example/demo/service/RiskScoreService.java
@@ -4,19 +4,7 @@ import com.example.demo.model.RiskScore;
 import java.util.List;
 
 public interface RiskScoreService {
-    RiskScore evaluateVisitor(Long visitorId);
-
-    RiskScore getScoreForVisitor(Long visitorId);
-
+    RiskScore evaluateVisitor(Long id);
+    RiskScore getScoreForVisitor(Long id);
     List<RiskScore> getAllScores();
 }
-
-
-// package com.example.demo.service;
-// import com.example.demo.model.RiskScore;
-// import java.util.List;
-// public interface RiskScoreService {
-//     RiskScore evaluateRisk(Long visitorId);
-//     RiskScore getRiskScoreByVisitor(Long visitorId);
-//     List<RiskScore> getAllRiskScores();
-// }
--- src/main/java/com/example/demo/service/ScoreAuditLogService.java
+++ src/main/java/com/example/demo/service/ScoreAuditLogService.java
@@ -4,20 +4,7 @@ import com.example.demo.model.ScoreAuditLog;
 import java.util.List;
 
 public interface ScoreAuditLogService {
-    ScoreAuditLog logScoreChange(Long visitorId, Long riskRuleId, ScoreAuditLog log);
-
+    ScoreAuditLog logScoreChange(Long visitorId, Long ruleId, ScoreAuditLog log);
     ScoreAuditLog getLog(Long id);
-
     List<ScoreAuditLog> getLogsByVisitor(Long visitorId);
 }
-
-
-
-// package com.example.demo.service;
-// import com.example.demo.model.ScoreAuditLog;
-// import java.util.List;
-// public interface ScoreAuditLogService {
-//     ScoreAuditLog addAuditLog(Long visitorId, Long ruleId, ScoreAuditLog log);
-//     ScoreAuditLog getAuditLogById(Long id);
-//     List<ScoreAuditLog> getAuditLogsByVisitor(Long visitorId);
-// }
