89c2e1d7ea95be5bf2ff1af8954ef74c17638a46
bala-amypo <bala@amypo.in>
2025-12-26 13:38:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitLogController.java
+++ src/main/java/com/example/demo/controller/VisitLogController.java
@@ -1,34 +1,14 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.RiskScore;
-import com.example.demo.service.RiskScoreService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/risk-scores")
-@RequiredArgsConstructor
-@Tag(name = "RiskScoreController")
-public class RiskScoreController {
-
-    private final RiskScoreService riskScoreService;
-
-    @PostMapping("/evaluate/{visitorId}")
-    public ResponseEntity<RiskScore> evaluate(@PathVariable Long visitorId) {
-        return ResponseEntity.ok(riskScoreService.evaluateVisitor(visitorId));
-    }
-
-    @GetMapping("/{visitorId}")
-    public ResponseEntity<RiskScore> get(@PathVariable Long visitorId) {
-        return ResponseEntity.ok(riskScoreService.getScoreForVisitor(visitorId));
-    }
+@RequestMapping("/api/visitlogs")
+public class VisitLogController {
 
     @GetMapping
-    public ResponseEntity<List<RiskScore>> all() {
-        return ResponseEntity.ok(riskScoreService.getAllScores());
+    public String test() {
+        return "VisitLog API working";
     }
 }
 
