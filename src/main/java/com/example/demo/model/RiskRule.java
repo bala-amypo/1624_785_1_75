a454cbdb7c82a162b855deede776e15f5f03798a
bala-amypo <bala@amypo.in>
2025-12-26 12:18:48 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRule.java
+++ src/main/java/com/example/demo/model/RiskRule.java
@@ -1,33 +1,59 @@
 package com.example.demo.model;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.time.LocalDateTime;
+import javax.persistence.*;
+
 @Entity
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RiskRule {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    
     private String ruleName;
     private String ruleType;
-    private Integer threshold;
     private Integer scoreImpact;
-    private LocalDateTime createdAt;
-    @PrePersist
-    void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
+    private Integer threshold;
 }
+
+
+
+// package com.example.demo.model;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.PrePersist;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+// import java.time.LocalDateTime;
+// @Entity
+// @Getter
+// @Setter
+// @Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class RiskRule {
+//     @Id
+//     @GeneratedValue
+//     private Long id;
+//     @Column(unique = true)
+//     private String ruleName;
+//     private String ruleType;
+//     private Integer threshold;
+//     private Integer scoreImpact;
+//     private LocalDateTime createdAt;
+//     @PrePersist
+//     void onCreate() {
+//         createdAt = LocalDateTime.now();
+//     }
+// }
