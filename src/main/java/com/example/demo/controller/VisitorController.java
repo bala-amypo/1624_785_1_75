a1b4cde2f6a399805ee63430649999c2c3d97559
bala-amypo <bala@amypo.in>
2025-12-26 15:08:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -3,63 +3,28 @@ package com.example.demo.controller;
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/visitors")
-@RequiredArgsConstructor
-@Tag(name = "VisitorController")
+@Tag(name = "Visitor")
 public class VisitorController {
 
-    // Made public or accessible for test setup constructing it manually
-    private final VisitorService visitorService;
+    private final VisitorService service;
 
-    @PostMapping
-    public ResponseEntity<Visitor> create(@RequestBody Visitor visitor) {
-        return ResponseEntity.ok(visitorService.createVisitor(visitor));
+    public VisitorController(VisitorService service) {
+        this.service = service;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Visitor> get(@PathVariable Long id) {
-        return ResponseEntity.ok(visitorService.getVisitor(id));
+    public ResponseEntity<Visitor> create(Visitor v) {
+        return ResponseEntity.ok(service.createVisitor(v));
     }
 
-    @GetMapping
+    public ResponseEntity<Visitor> get(long id) {
+        return ResponseEntity.ok(service.getVisitor(id));
+    }
+
     public ResponseEntity<List<Visitor>> all() {
-        return ResponseEntity.ok(visitorService.getAllVisitors());
+        return ResponseEntity.ok(service.getAllVisitors());
     }
 }
-
-// package com.example.demo.controller;
-// import com.example.demo.model.Visitor;
-// import com.example.demo.service.VisitorService;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import java.util.List;
-// @RestController
-// @RequestMapping("/api/visitors")
-// public class VisitorController {
-//     private final VisitorService service;
-//     public VisitorController(VisitorService service) {
-//         this.service = service;
-//     }
-//     @PostMapping
-//     public Visitor createVisitor(@RequestBody Visitor visitor) {
-//         return service.createVisitor(visitor);
-//     }
-//     @GetMapping("/{id}")
-//     public Visitor getVisitor(@PathVariable Long id) {
-//         return service.getVisitorById(id);
-//     }
-//     @GetMapping
-//     public List<Visitor> getAllVisitors() {
-//         return service.getAllVisitors();
-//     }
-// }
