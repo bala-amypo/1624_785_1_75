c7e247db1d65b2bb3b56209b514b00d2573a88ba
bala-amypo <bala@amypo.in>
2025-12-26 13:08:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -3,38 +3,35 @@ package com.example.demo.controller;
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/visitors")
-@Tag(name = "Visitors")
+@RequiredArgsConstructor
+@Tag(name = "VisitorController")
 public class VisitorController {
-    
+
+    // Made public or accessible for test setup constructing it manually
     private final VisitorService visitorService;
-    
-    public VisitorController(VisitorService visitorService) {
-        this.visitorService = visitorService;
-    }
-    
+
     @PostMapping
     public ResponseEntity<Visitor> create(@RequestBody Visitor visitor) {
         return ResponseEntity.ok(visitorService.createVisitor(visitor));
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<Visitor> get(@PathVariable Long id) {
         return ResponseEntity.ok(visitorService.getVisitor(id));
     }
-    
+
     @GetMapping
     public ResponseEntity<List<Visitor>> all() {
         return ResponseEntity.ok(visitorService.getAllVisitors());
     }
 }
-
-
 
 // package com.example.demo.controller;
 // import com.example.demo.model.Visitor;
