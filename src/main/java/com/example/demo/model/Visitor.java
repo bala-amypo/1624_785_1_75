a66ada7fff9010c787e54fc3c82e0f25e6d0baea
bala-amypo <bala@amypo.in>
2025-12-26 12:21:43 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,43 +1,67 @@
 package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
+import javax.persistence.*;
+
 @Entity
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Visitor {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String fullName;
     private String phone;
-    private LocalDateTime createdAt;
-    @PrePersist
-    void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
-    public Long getId() {
-     return id; 
-     }
-    public String getFullName() { 
-    return fullName;
-     }
-    public String getPhone() { 
-    return phone; 
-    }
-    public LocalDateTime getCreatedAt() {
-     return createdAt; 
-     }
-     public void setFullName(String fullName) {
-     this.fullName = fullName; 
-     }
-    public void setPhone(String phone) { 
-    this.phone = phone; 
-    }
+    private String email;
+    private String idProof;
 }
+
+// package com.example.demo.model;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.PrePersist;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.NoArgsConstructor;
+// import java.time.LocalDateTime;
+// @Entity
+// @Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Visitor {
+//     @Id
+//     @GeneratedValue
+//     private Long id;
+//     private String fullName;
+//     private String phone;
+//     private LocalDateTime createdAt;
+//     @PrePersist
+//     void onCreate() {
+//         createdAt = LocalDateTime.now();
+//     }
+//     public Long getId() {
+//      return id; 
+//      }
+//     public String getFullName() { 
+//     return fullName;
+//      }
+//     public String getPhone() { 
+//     return phone; 
+//     }
+//     public LocalDateTime getCreatedAt() {
+//      return createdAt; 
+//      }
+//      public void setFullName(String fullName) {
+//      this.fullName = fullName; 
+//      }
+//     public void setPhone(String phone) { 
+//     this.phone = phone; 
+//     }
+// }
