4187e10ea9c27535257a827389fc19fae0af8b74
bala-amypo <bala@amypo.in>
2025-12-27 04:15:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/ScoreAuditLogController.java
+++ src/main/java/com/example/demo/controller/ScoreAuditLogController.java
@@ -1,30 +1,34 @@
-package com.example.demo.controller;
 
-import com.example.demo.model.ScoreAuditLog;
-import com.example.demo.service.ScoreAuditLogService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
 
-import java.util.List;
 
-@Tag(name = "ScoreAuditLog")
-public class ScoreAuditLogController {
 
-    private final ScoreAuditLogService service;
+// package com.example.demo.controller;
 
-    public ScoreAuditLogController(ScoreAuditLogService service) {
-        this.service = service;
-    }
+// import com.example.demo.model.ScoreAuditLog;
+// import com.example.demo.service.ScoreAuditLogService;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import org.springframework.http.ResponseEntity;
 
-    public ResponseEntity<ScoreAuditLog> create(long v, long r, ScoreAuditLog log) {
-        return ResponseEntity.ok(service.logScoreChange(v, r, log));
-    }
+// import java.util.List;
 
-    public ResponseEntity<ScoreAuditLog> get(long id) {
-        return ResponseEntity.ok(service.getLog(id));
-    }
+// @Tag(name = "ScoreAuditLog")
+// public class ScoreAuditLogController {
 
-    public ResponseEntity<List<ScoreAuditLog>> logsByVisitor(long id) {
-        return ResponseEntity.ok(service.getLogsByVisitor(id));
-    }
-}
+//     private final ScoreAuditLogService service;
+
+//     public ScoreAuditLogController(ScoreAuditLogService service) {
+//         this.service = service;
+//     }
+
+//     public ResponseEntity<ScoreAuditLog> create(long v, long r, ScoreAuditLog log) {
+//         return ResponseEntity.ok(service.logScoreChange(v, r, log));
+//     }
+
+//     public ResponseEntity<ScoreAuditLog> get(long id) {
+//         return ResponseEntity.ok(service.getLog(id));
+//     }
+
+//     public ResponseEntity<List<ScoreAuditLog>> logsByVisitor(long id) {
+//         return ResponseEntity.ok(service.getLogsByVisitor(id));
+//     }
+// }
